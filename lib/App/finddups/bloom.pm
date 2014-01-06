ac9867a0a83d920d6931ebf178a4811ed37df642
Reini Urban <rurban@cpanel.net>
2014-01-06 15:07:00 -0600

    seperate module from script to please CPAN and perlcritic
    conway didnt know about modulinos, and has some wrong opinions on style.
    anyway: perlcritic violations down from 88 to 51

---
+++ lib/App/finddups/bloom.pm
@@ -0,0 +1,161 @@
+package App::finddups::bloom;
+
+use strict;
+use warnings;
+use 5.008;
+our $VERSION = '0.01';
+
+# local::lib hack, dfw-specific only
+# push @INC, "perl5/lib/perl5:perl5/lib/perl5/x86_64-linux-gnu-thread-multi";
+
+=head1 NAME
+
+App::finddups::bloom
+
+=head1 SYNOPSIS
+
+    use File::Find;
+    use App::finddups::bloom;
+
+    App::finddups::bloom::options('0' => 1);
+
+    File::Find::find(\&App::finddups::bloom::wanted, $ARGV[0]);
+
+=head1 DESCRIPTION
+
+Finds and prints most file duplicates under the given directory via bloom filters.
+Ignores symlinks and hardlinks.
+
+=head1 OPTIONS
+
+Options are passed to the module as hash to the subroutine C<options>.
+The keys are:
+
+=over 4
+
+=item 0 - quote unprintable filename characters for xargs -0
+
+=item bloomsize - expected number of files, defaults to 1_000_000;
+
+=item bloomsize2 - expected number of duplicates, defaults to 2000;
+
+=item bloomerr - wanted error rate, defaults to 0.01 i.e. 1%
+
+=item maxcrc - maximal file content size which is checked
+
+=item verbose - prints header and footer prefixed with #
+
+=item help - prints short usage
+
+=back
+
+=cut
+
+use Bloom::Faster ();
+use Digest::CRC ();
+use File::Find ();
+
+my $maxcrc = 1_000_000;
+my $bloomsize = 100_000;
+my $bloomsize2 = 2000;
+my $bloomerr = 0.01;
+
+my %opts;
+my $s; # global crc buffer
+# expected elements + error rate (not using options yet)
+my $size = Bloom::Faster->new({'n' => $bloomsize, 'e' => $bloomerr});
+# less expected same-size entries
+my $hash = Bloom::Faster->new({'n' => $bloomsize2, 'e' => $bloomerr});
+my $crc = Digest::CRC->new('type' => "crc64");
+
+sub wanted {
+  return if (!-f $_) or (-l $_); # skip symlinks and non-files
+  return if (stat($_))[3] > 1;   # also skip hardlinks not using a inode hash, the fs already stores nlinks
+  if ($size->add(-s _)) {        # only compare same filesizes
+    my $c;
+    open my $f,'<',$_ or return;
+    if (-s _ < $maxcrc) {
+      $c = $crc->addfile($f);
+    } else {
+      sysread $f, $s, $maxcrc; # only check the first 1 million bytes
+      $c = $crc->add($s);
+    }
+    if ($hash->add($c)) {
+      if ($opts{0}) {
+        print B::cstring($File::Find::name)."\000\n";
+      } else {
+        print $File::Find::name."\n";
+      }
+    }
+    close $f or return;
+  }
+  return 1;
+}
+
+sub options {
+  %opts = @_;
+  if ($opts{0}) {
+    require B;
+  }
+  return;
+}
+
+END {
+  if ($opts{'verbose'}) {
+    print "\n#bloom filter stats: ", $size->key_count, " size keys, ", $size->capacity, " size capacity\n";
+    print   "#                    ", $hash->key_count, " hash keys, ", $hash->capacity, " hash capacity\n";
+  }
+}
+
+1;
+
+=head1 INTERNALS
+
+This code doesn't follow the contest rules, as it doesn't store the source links. 
+It only checks and prints duplicates, and therefore can use fast and memory efficient
+Bloom filters.
+
+The default accuracy is 99.99% for 1 million files which should satisfy all
+practical purposes. And if you care for more run it a second time, combined with rm.
+It is fast because it uses bloom filters not hashes, hence the 99.99%
+and not 100% and it doesn't print the source for the duplicates, only the
+duplicates.
+
+To remove the duplicates apply C<rm> to the list of entries, such as
+
+    finddups-bloom -0 | xargs -0 rm
+
+It needs between 20MB and 55MB memory, compared to 333MB with the reference perl5
+code using perl hashes.
+
+cloc: 57
+
+$ perlcritic finddups-bloom
+finddups-bloom source OK
+
+=head1 wanted
+
+callback for L<Find::File>, implementing the module logic
+
+=head1 options
+
+store options hash from the script
+
+=head1 LICENSE
+
+Written and copyright 2014 by Reini Urban rurban@cpanel.net
+for the Dallas/Fort Worth Perl Mongers dfw dedup contest 2014
+http://dfw.pm.org/
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of Perl5, which is either:
+
+a) the GNU General Public License as published by the Free
+   Software Foundation; either version 1, or (at your option) any
+   later version, or
+
+b) the "Artistic License" which comes with this kit.
+
+=head1 SEE ALSO
+
+L<finddups-bloom>
--- test.pl
+++ test.pl
@@ -1,4 +1,7 @@
 BEGIN { print "1..1\n"; $/ = ""; }
-my $s = `$^X script/finddups-bloom t/dedup`;
+die "ok 1  #skip MSWin32 tests\n" if $^O eq 'MSWin32';
+
+my $X = $^X =~ m/\s/ ? qq{"$^X"} : $^X;
+my $s = `$X -Mblib script/finddups-bloom t/dedup`;
 chomp $s;
 print "ok 1\n" if $s eq "t/dedup/t2/f2";
